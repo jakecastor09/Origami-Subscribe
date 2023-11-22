15803837de1af698e077c4f09c6ac64d38b5c343
jakecastor09 <rencelaw70@gmail.com>
2023-11-22 14:51:52 +0800

    Add Validation freemium form 1 and 2

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const stepButtons = document.querySelectorAll('.step-button');
 const progress = document.querySelector('#progress');
 
@@ -14,3 +16,42 @@ Array.from(stepButtons).forEach((button, index) => {
     });
   });
 });
+
+// Validation
+
+// Sequence Number Form 1
+function validateCheckbox() {
+  const btnNext1 = document.querySelector('.btn-sequence-number');
+  const checkbox = document.querySelector('.form-check-input');
+  btnNext1.disabled = !checkbox.checked;
+}
+
+// Sequence Number Form 2
+function validateInputs() {
+  const inputFieldSchoolName2 = document.querySelector('.school-name-2');
+  const inputFieldSchoolAcronymn2 =
+    document.querySelector('.school-acronymn-2');
+  const inputFieldSchoolID2 = document.querySelector('.school-id-2');
+  const inputFieldSchoolAddress2 = document.querySelector('.school-address-2');
+  const inputFieldMobileNumber2 = document
+    .querySelector('.mobile-number-2')
+    .value.trim();
+  const inputFieldEmail2 = document.querySelector('.email-2').value.trim();
+
+  const btnNext2 = document.querySelector('.btn-school-information');
+
+  const isValidNumber = /^\d{11}$/.test(inputFieldMobileNumber2);
+  const isValidEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(inputFieldEmail2);
+  if (
+    inputFieldSchoolName2.value.trim('') !== '' &&
+    inputFieldSchoolAcronymn2.value.trim('') !== '' &&
+    inputFieldSchoolID2.value.trim('') !== '' &&
+    inputFieldSchoolAddress2.value.trim('') !== '' &&
+    isValidNumber &&
+    isValidEmail
+  ) {
+    btnNext2.disabled = false;
+  } else {
+    btnNext2.disabled = true;
+  }
+}
